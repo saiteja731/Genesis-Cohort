f22ed038841df0be8659bb74c426214afa95851f
Chatha Sai Teja <saitejajnv123@gmail.com>
2026-07-31 08:07:54 +0530

    Update Day 1 SQL Practice.sql

--- Day-1 SQL/Day 1 SQL Practice.sql
+++ Day-1 SQL/Day 1 SQL Practice.sql
@@ -1,16 +1,30 @@
 -- Databricks notebook source
-
+//Display the CustomerName, City, Balance, and LoanAmount.
+  
 select customerName, city, Balance,LoanAmount from customer;
 
+//Display the unique cities.
+  
 select distinct city from customer;
 
+//Sort customers by LoanAmount (Highest to Lowest).
+  
 select * from customer
 order by LoanAmount desc;
 
+//Show only the top 5 customers based on LoanAmount
+  
 select * from customer
 order by LoanAmount desc
 limit 5;
 
+//Find:
+Total number of customers
+Total Loan Amount
+Minimum Balance
+Maximum Balance
+Average Balance
+  
 select sum(LoanAmount) from customer;
 select min(Balance) from customer;
 select max(Balance) from customer;
